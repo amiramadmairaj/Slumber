d71f9e93b35c1cbf65d207026ba50a5f0e4bd219
reishaladwa <86329309+reishaladwa@users.noreply.github.com>
2023-02-07 07:24:19 -0800

    Initial Commit

---
+++ SlumbeR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlumbeR
+//
+//  Created by Reisha Ladwa on 2/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlumbeR/SlumbeRApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlumbeRApp.swift
+//  SlumbeR
+//
+//  Created by Reisha Ladwa on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SlumbeRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
