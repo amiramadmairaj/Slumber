f7b76ffb08e9a66a0498daa1b6ab7cc15c38dac0
reishaladwa <86329309+reishaladwa@users.noreply.github.com>
2023-02-14 18:00:21 -0800

    small ui change - reisha

--- SlumbeR/Views/Main/TabBar.swift
+++ SlumbeR/Views/Main/TabBar.swift
@@ -10,17 +10,18 @@ import SwiftUI
 struct TabBar: View {
     var body: some View {
         TabView {
-//            HomeView()
-//                .tabItem{
-//                    Label("Home", systemImage: "person")
-//                }
             ContentView()
                 .tabItem{
-                    Label("Track", systemImage: "square.fill.text.grid.1x2")
+                    Label("Home", systemImage: "powersleep")
+                }
+            TipsView()
+                .tabItem{
+                    Label("Tips", systemImage: "lightbulb")
                 }
         }
     }
 }
+
 
 struct TabBar_Previews: PreviewProvider {
     static var previews: some View {
---
+++ SlumbeR/Views/Main/TipsView.swift
@@ -0,0 +1,20 @@
+//
+//  TipsView.swift
+//  SlumbeR
+//
+//  Created by Reisha Ladwa on 2/14/23.
+//
+
+import SwiftUI
+
+struct TipsView: View {
+    var body: some View {
+        Text("drink water and sleep more")
+    }
+}
+
+struct TipsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TipsView()
+    }
+}
