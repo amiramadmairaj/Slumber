febe314a4d23a66986b27cbd010011c27b5630b1
Amir Mairaj <amairaj@uci.edu>
2023-03-01 13:28:35 -0800

    Made changes to Main Screen, added graph collected from users HealthKit Data

--- SlumbeR/Views/Main/MainMenu.swift
+++ SlumbeR/Views/Main/MainMenu.swift
@@ -6,73 +6,46 @@
 //
 import SwiftUI
 import HealthKit
-import Charts
+import SwiftUICharts
 
-struct SleepDataChart:
-    View {
-    // Initialize HealthKit store
+
+struct SleepDataChart: View {
     let healthStore = HKHealthStore()
     let sleepType = HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!
-       
-    // Define properties to hold sleep data
-    @State var sleepSamples: [HKCategorySample] = []
-    @State var sleepHours: [Double] = []
     
-    // Define the start and end date to fetch sleep data
-    let startDate = Calendar.current.date(byAdding: .day, value: -7, to: Date())!
-    let endDate = Date()
-    
-
-    func greetingLogic() -> String {
+    @State var sleepData: [(Date, Double)] = []
         
-      let hour = Calendar.current.component(.hour, from: Date())
-      
-      let NEW_DAY = 0
-      let NOON = 12
-      let SUNSET = 18
-      let MIDNIGHT = 24
-      
-      var greetingText = "Hello" // Default greeting text
-      switch hour {
-      case NEW_DAY..<NOON:
-          greetingText = "Good Morning"
-      case NOON..<SUNSET:
-          greetingText = "Good Afternoon"
-      case SUNSET..<MIDNIGHT:
-          greetingText = "Good Evening"
-      default:
-          _ = "Hello"
-      }
-      
-      return greetingText
-    }
-    
-    var body: some View {
-        // Fetch sleep data from HealthKit
-        VStack {
-            if !sleepHours.isEmpty == false {
+        var body: some View {
+//                    HStack (alignment: .top){ // << moved this up to ZStack
+//                        Text("Top Text")
+//                            .fontWeight(.light)
+//                            .multilineTextAlignment(.center)
+//                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+//                            .font(.body)
+//                    }
+                   
+            VStack {
                 Text("\(greetingLogic()), \(name)")
-                Text(" YEET, IMAGINE A GRAPH OF SLEEP DATA IS HERE ")
-                // Graph Sleep Health Data Here (7 day period)
+                    .multilineTextAlignment(.center)
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 30)
+                    .foregroundColor(themeColor)
+                    .shadow(color: themeColor, radius: 16)
+                    .padding(.top, 60)
+                    .padding(.bottom, 30)
+                    .font(.system(size: 40, weight: .bold, design: .default))
+                //FIGURE OUT WHY THIS IS SURROUNDED BY A BOX, ALSO IS THIS A GRAPH?
+                // PieChartView(data: sleepData.map { $0.1 }, title: "Sleep Data").padding()
+                LineChartView(data: sleepData.map { $0.1 }, title: "Sleep Data").padding()
                 
                 
-           
-                
-            } else {
-                Text("No sleep data found\n Please check your settings to ensure SlumbeR has access to HealthKit sleep data")
+                Button("Refresh Sleep Data") {
+                    authorizeHealthKit()
+                    fetchSleepData()
+                }
             }
         }
-        .onAppear {
-            authorizeHealthKit()
-            fetchSleepData()
-        }
-    }
-    
-    // Request authorization to access HealthKit data
     func authorizeHealthKit() {
-        let sleepType = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!
-        
-        healthStore.requestAuthorization(toShare: [], read: [sleepType]) { (success,error) in
+        self.healthStore.requestAuthorization(toShare: [], read: [self.sleepType]) { (success,error) in
             if !success {
                 print("Authorization failed: \(error?.localizedDescription ?? "Unknown error")")
             }
@@ -80,31 +53,52 @@ struct SleepDataChart:
     }
     
     func fetchSleepData() {
-           let calendar = Calendar.current
-           let now = Date()
-           let oneWeekAgo = calendar.date(byAdding: .day, value: -7, to: now)!
-           let predicate = HKQuery.predicateForSamples(withStart: oneWeekAgo, end: now, options: .strictStartDate)
+        let calendar = Calendar.current
+        let now = Date()
+        let oneWeekAgo = calendar.date(byAdding: .day, value: -7, to: now)!
+        let predicate = HKQuery.predicateForSamples(withStart: oneWeekAgo, end: now, options: .strictStartDate)
+        
+        let query = HKSampleQuery(sampleType: self.sleepType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { (query, samples, error) in
+            if let samples = samples as? [HKCategorySample] {
+                let data = samples.map { sample -> (Date, Double) in
+                    let value = sample.value == HKCategoryValueSleepAnalysis.inBed.rawValue ? 1.0 : 0.0
+                    return (sample.startDate, value)
+                }
+                self.sleepData = data
+            }
+        }
+        
+        self.healthStore.execute(query)
+    }
+}
 
-           let query = HKSampleQuery(sampleType: sleepType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { (query, samples, error) in
-               if let samples = samples as? [HKCategorySample] {
-                   self.processSleepData(samples)
-               }
-           }
 
-           healthStore.execute(query)
-       }
-       
-       func processSleepData(_ samples: [HKCategorySample]) {
-           let sleepData = samples.map { sample -> (Date, Double) in
-               let value = sample.value == HKCategoryValueSleepAnalysis.inBed.rawValue ? 1.0 : 0.0
-               return (sample.startDate, value)
-           }
-           
-           
-       }
-       
-   }
-    
+
+
+func greetingLogic() -> String {
+  let hour = Calendar.current.component(.hour, from: Date())
+  let NEW_DAY = 0
+  let NOON = 12
+  let SUNSET = 18
+  let MIDNIGHT = 24
+  var greetingText = "Hello" // Default greeting text
+  switch hour {
+  case NEW_DAY..<NOON:
+      greetingText = "Good Morning"
+  case NOON..<SUNSET:
+      greetingText = "Good Afternoon"
+  case SUNSET..<MIDNIGHT:
+      greetingText = "Good Evening"
+  default:
+      _ = "Hello"
+  }
+  
+  return greetingText
+}
+
+
+
+
     
 
 struct SleepDataChart_Previews: PreviewProvider {
