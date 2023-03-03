b43755df9b3788f2ebb73fd4ce49393a3a4b6c47
Anu <anuragkarra@gmail.com>
2023-03-03 02:58:49 -0800

    added hardcoded predict function

--- SlumbeR/Views/Main/utils.swift
+++ SlumbeR/Views/Main/utils.swift
@@ -2,63 +2,55 @@
 //  utils.swift
 //  SlumbeR
 //
-//  Created by slmrc on 2/26/23.
+//  Created by Anu on 2/26/23.
 //
 
-// This file is completely broken, still working on it
+import CoreML
 
+func get_efficiency(age: Int,
+                    gender: String,
+                    sleepduration: Double,
+                    remsleeppercentage: Double,
+                    deepsleeppercentage: Double,
+                    smokingstatus: String,
+                    exercisefrequency: String) -> Double {
+    
+    
+    do {
+        
+        // hard coded values
+        // still need to get real values tranlated to correct types
+        let age = Double("65")
+        let gender = "Female"
+        let sleep_dur = Double("6")
+        let rem_per = Double("18")
+        let deep_per = Double("70")
+        let smoke = "Yes"
+        let ex_freq = Double("3")
+        
+        
+        
+        guard let model = try? SleepE(configuration: MLModelConfiguration()) else {
+            fatalError("Unable to load model")
+        }
+        
+        let input = SleepEInput(Age: age!,
+                                Gender: gender,
+                                Sleepduration: sleep_dur!,
+                                REMsleeppercentage: rem_per!,
+                                Deepsleeppercentage: deep_per!,
+                                Smokingstatus: smoke,
+                                Exercisefrequency: ex_freq!)
 
+        guard let output = try? model.prediction(input: input) else {
+            fatalError("Unable to make prediction")
+        }
+        
+        return output.Sleepefficiency
 
-//import CreateML
-//import Foundation
-//
-//let data = try MLDataTable(contentsOf: URL(fileURLWithPath: "/Users/slmrc/Downloads/Sleep_Emod.csv")).dropMissing()
-//let (trainingData, testingData) = data.randomSplit(by: 0.8)
-//
-//let regressor = try MLRegressor(trainingData: trainingData, targetColumn: "Sleepefficiency")
-//
-//let evaluationMetrics = regressor.evaluation(on: testingData)
-//print(evaluationMetrics.rootMeanSquaredError)
-//print(evaluationMetrics.maximumError)
-//
-//let metadata = MLModelMetadata(author: "Anu", shortDescription: "A model trained to predict optimum sleep times", version: "1.0")
-//
-//try regressor.write(to: URL(fileURLWithPath: "/Users/slmrc/Desktop/SleepE.mlmodel"), metadata: metadata)
-//
-//
-//
-//
-//let model = MLRegressor(contentsOf: URL(fileURLWithPath: "/Users/slmrc/Desktop/SleepE.mlmodel"))
-//
-//
-//
-//
-//do {
-//    let age = "65"
-//    let gender = "Female"
-//    let sleep_dur = "6"
-//    let rem_per = "18"
-//    let deep_per = "70"
-//    let smoke = "Yes"
-//    let ex_freq = "3"
-//
-//    let inputCSV = """
-//Age,Gender,Sleepduration,REMsleeppercentage,Deepsleeppercentage,Smokingstatus,Exercisefrequency
-//65,Female,6,18,70,Yes,3
-//"""
-//
-//    let input_data = MLDataTable(contentsOf: inputCSV.data(using: .utf8)!, options: .delimiter(","))
-//
-//    let prediction = try model.predictions(Age: age,
-//                                          Gender: gender,
-//                                          Sleepduration: sleep_dur,
-//                                          REMsleeppercentage: rem_per,
-//                                          Deepsleeppercentage: deep_per,
-//                                          Smokingstatus: smoke,
-//                                          Exercisefrequency: ex_freq)
-//    print(prediction)
-//} catch {
-//    print("prediction error")
-//}
-//
+    } catch {
+        return -1
+    }
+}
+
 
